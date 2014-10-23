dc4080837c7752cdf86c2075aa9b1464cd8be148
pavankumar2203 <pavankumar2203@gmail.com>
2014-10-23 19:09:55 -0400

    added accordion to ballot information

--- js/blscript.js
+++ js/blscript.js
@@ -6,6 +6,8 @@ $(function(){
 				this.getUrl = config.getUrl; // location to server
 				this.buildLayout();
 				this.addListener();
+    				
+
 			},
 			buildLayout: function(){
 				var layout = '<div id="pl-targetInner"> \
@@ -20,8 +22,8 @@ $(function(){
 						          </div> \
 						        </div> \
 						      </div> \
-						      </br> \
-						      <div id="accordion" class="panel panel-primary"> </div> \
+							  <br> \
+							  <div id="accordion" class="panel panel-primary"> </div> \
 						    </div>';
 	  			this.$target.append(layout);
 			},
@@ -31,11 +33,11 @@ $(function(){
 			},
 			sendAjaxRequest: function () {
 				$('#pl-search').append(' <span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>'); //add loading
-				$('#accordion').empty();// remove location
+				$('#accordion').empty(); // remove location
 				
 				var address = $('#pl-userInput').val(); //get address
-				var getUrl = plWidget.getUrl;
-				var GCurl = getUrl+address;
+				//var getUrl = plWidget.getUrl;
+				var GCurl = 'https://www.googleapis.com/civicinfo/v2/voterinfo?address=5530%20fifth%20avenue%20pittsburgh&key=AIzaSyBGtYVq_OZ35H4BY-r4IAx5cYAVTuOG7rQ';
 				
 
 				$.ajax({ // send ajax request
@@ -50,44 +52,45 @@ $(function(){
 				var $targetInner = $('#accordion');
 				$.get(GCurl,function(data){
 					
-					if(data.length > 3){ // validate data
+					var buildHTML = "";
+					if(data.contests.length > 0){ // validate data
 
-						var edata = jQuery.parseJSON(data); //convert json to javascript object
+						var edata = data; //convert json to javascript object
 						
-						
-
-							
 							for (var i = edata.contests.length - 1; i >= 0; i--) { // loop through data
-								var electionType = edata.contests[i].type;
-								
+								var electionType = edata.contests[i].type;								
 								var office = edata.contests[i].office;
-								$targetInner.append('<h3  class="panel-heading">'+office+' ('+electionType +')'+'</h3>');
+								buildHTML += '<h2  class="ctrl">'+office+' ('+electionType +')'+'</h2>';
 								var district = edata.contests[i].district;
-
-
-								$targetInner.append('<h5 class="panel-heading">'+ district.name+' ('+district.scope+')'+'</h5>');
-
 								var candidates = edata.contests[i].candidates;
+								buildHTML += '<div class="pnl">';
+								buildHTML += '<h5 >'+ district.name+' ('+district.scope+')'+'</h5>';
 								for (var j = 0; j <= candidates.length - 1; j++) {
 									var party = candidates[j].party;
 									party = party.replace("Democratic", "D");
 									party = party.replace("Republican", "R");
-
-									$targetInner.append('<h4  class="panel-body">'+'&#x25A2;' +' ' + candidates[j].name +' ('+party+')</h4>');
-
+									buildHTML += '<a href="http://localhost:8080/MVAWigets/blwidget.php"><h4  class="accordian-content panel-body">'+'&#x25A2;' +' ' + candidates[j].name +' ('+party+')</h4></a>';
 								};
-
-								$targetInner.append('<h4  class="panel-body">'+ '&#x25A2;' +' ' + '___________ ' +'(Write-in)'+'</h4>');
+								buildHTML += '<h4  class="accordian-content panel-body">'+ '&#x25A2;' +' ' + '___________ ' +'(Write-in)'+'</h4>';
+								buildHTML += '</div>';
 							};
+					
 						
 					} else { // if no data
-						$targetInner.append('<h3 class="location-item">invalid input</h3>');
+						buildHTML += '<h2 class="location-item">invalid input</h1>';
 					}
 					$('.glyphicon-refresh').remove();// remove loading
-
 					
-
+					
+				$targetInner.append(buildHTML);
+					
+					$( "#accordion" ).accordion();
+					
 				});
+
+				
+				
+				
 			},
 			getComma: function (zip){
 				if(zip){
@@ -103,7 +106,7 @@ $(function(){
 
 		plWidget.init({ //initialize with target and location to GC server app
 			target: '#target-practice',
-			getUrl: 'http://julian-nworb.com/PollingLocationWidget/server/getcivic.php?a='
+			getUrl: 'http://localhost:8080/AjaxTemplate/server/server.php?a='
 		});
 
 	})(); // end of self invoking function
