499655923febb3476b29b8471597ca803c7052c5
carinalee <carinalee0920@gmail.com>
2014-11-12 09:18:39 -0500

    ci update br

--- js/ciscript.js
+++ js/ciscript.js
@@ -100,7 +100,7 @@ $(function(){
 
 								$top.append('<h2>Candidate Information</h2>');
 								$top.append('<div id="namePanel" class="panel panel-primary"></div><br>')
-								$right.append('<h3 style="color:#064479">'+candidates[j].name+'</h2>');
+								$right.append('<h3 style="color:#064479">'+candidates[j].name+'<br><br><br><br><br></h2>');
 								
 
 								if (candidates[j].party.length > 0)
