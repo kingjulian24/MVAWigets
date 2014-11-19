cd53a341cd64a02e24c0175c85fb761d3ba970a7
laden88 <laden88@gmail.com>
2014-11-19 12:06:42 -0500

    done3

--- js/riscript.js
+++ js/riscript.js
@@ -91,6 +91,7 @@ $(function () {
                 var candidateName = name;
 
                 $.get(GCurl, function (data) {
+                var edata = data;
                     if (edata.offices.length > 0) { // validate data
 
 
