754a1543dfa4591857e7b1a656c713eba52742aa
Julian Brown <brownjulian2@gmail.com>
2014-11-29 21:33:59 -0500

    add bootstrap js

--- blwidget.php
+++ blwidget.php
@@ -7,6 +7,8 @@
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+  <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="http://rawgit.com/kingjulian24/MVAWigets/master/js/blscript2.js"></script>
 
 
