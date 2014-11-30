3c160016f185e89099c5e65e30133055eafa0e28
Julian Brown <brownjulian2@gmail.com>
2014-11-30 16:08:27 -0500

    update ri

--- riwidget.php
+++ riwidget.php
@@ -3,7 +3,8 @@
     <div id="target-practice"></div>
 
 <?php include('layout/footer.php'); ?>
-
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
  <script type="text/javascript" src="http://rawgit.com/kingjulian24/MVAWigets/master/js/riscript3.js"></script>
 
 
