d80c2491261645f92a6c92dec7191039b543997e
Julian Brown <brownjulian2@gmail.com>
2014-11-30 16:07:07 -0500

    update ri

--- js/riscript3.js
+++ js/riscript3.js
@@ -27,7 +27,7 @@ $(function () {
            });
 
 				  this.addListener(); // listen for search
-          //this.sendAjaxRequest(); // initialize search
+          this.sendAjaxRequest(); // initialize search
           this.hideMenu();
 		      this.clearInitAddress();  // clear input value
 
@@ -71,6 +71,30 @@ $(function () {
                             </div> \
                           </div> \
                      </div>';
+
+
+                     //modal
+                     layout	+= '<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+                         <div class="modal-dialog">\
+                           <div class="modal-content">\
+                             <div class="modal-header">\
+                               <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\
+                               <h4 class="modal-title" id="myModalLabel">Modal title</h4>\
+                             </div>\
+                             <div class="modal-body">\
+                               <div class="row">\
+                                 <div class="col-xs-4 photoUrl">\
+                                 </div>\
+                                 <div class="col-xs-8 candidateInfo">\
+                                 </div>\
+                               </div>\
+                             </div>\
+                             <div class="modal-footer">\
+                               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+                             </div>\
+                           </div>\
+                         </div>\
+                       </div>';
                 this.$target.append(layout);
           },
           hideMenu: function(){
@@ -107,6 +131,88 @@ $(function () {
               self.preDisplay(self.reps[4]);
             });
     			},
+
+          repListener:function(){
+            var self = this;
+              $('.rep-img').on('click', self.showRepInfo);
+          },
+          showRepInfo: function(){
+            $('.candidateInfo').html('');
+            $('#myModalLabel').html('');
+            $('.photoUrl').html('');
+
+            var photo = $(this).data('photourl');
+            var position = $(this).data('position');
+            var phones = $(this).data('phones');
+            var name = $(this).data('name');
+            var party = $(this).data('party');
+            var phonesArray = phones.split(',');
+            var phoneString = '';
+            for(var j = 0; j < phonesArray.length-1; j++){
+              phoneString += '<p><i class="fa fa-tty social-fa"></i>'+phonesArray[j]+'</p>';
+            }
+            var urls = $(this).data('urls');
+            var urlsArray = urls.split(',');
+            var urlString = '';
+            for(var u = 0; u < urlsArray.length-1; u++){
+              urlString += '<a href="'+urlsArray[u]+'"><i class="fa fa-bookmark social-fa"></i></a>';
+            }
+            var channels = $(this).data('channels');
+            var chanArray = channels.split(',');
+            var chanString = '';
+            var type = '';
+            var id = '';
+
+            for(var k = 0; k < chanArray.length-1; k++){
+              type = chanArray[k].split(' ')[0];
+              id   = chanArray[k].split(' ')[1];
+
+              if(type === 'Facebook') {
+                chanString += '<a href="https://www.facebook.com/' + id + '"><i class="fa fa-facebook-square social-fa"></i></a>';
+              } else if( type === 'Twitter') {
+                chanString += '<a href="https://www.Twitter.com/' + id + '"><i class="fa fa-twitter social-fa"></i></a>';
+              } else if( type === 'YouTube') {
+                chanString += '<a href="https://www.YouTube.com/' + id + '"><i class="fa fa-youtube-play social-fa"></i></a>';
+              }
+            }
+
+
+
+            $('#myModalLabel').html(position);
+            $('.candidateInfo').append('<h4>'+name+'</h4>');
+            $('.candidateInfo').append('<p>'+party+'</p>');
+            $('.candidateInfo').append(phoneString);
+            $('.candidateInfo').append(urlString);
+            $('.candidateInfo').append(chanString);
+
+
+            var photohtml = $('<img>',{
+              src: photo,
+            }).css('max-width','100%');
+
+            $('.photoUrl').append(photohtml);
+            var $icons = $('.social-fa');
+            var $youtube = $('.fa-youtube-play');
+            var $twitter = $('.fa-twitter');
+
+            $icons.css({
+              fontSize: '20px',
+              paddingRight: '10px'
+            });
+
+            $youtube.css({
+              color: 'red'
+            });
+
+            $twitter.css({
+              color: 'rgb(0,153,153)'
+            });
+
+            $('#myModal').modal('show');
+          },
+
+
+
           preDisplay: function(reps) {
             this.clearReps();
             this.display(reps);
@@ -285,11 +391,11 @@ $(function () {
                     id   = officials[i].channels[j].id || '';
 
                     if(type === 'Facebook') {
-                      channels += '<a href="https://www.facebook.com/' + id + '"><i class="fa fa-facebook-square social-fa"></i></a>';
+                      channels += 'Facebook '+id+',';
                     } else if( type === 'Twitter') {
-                      channels += '<a href="https://www.Twitter.com/' + id + '"><i class="fa fa-twitter social-fa"></i></a>';
+                      channels += 'Twitter '+id+',';
                     } else if( type === 'YouTube') {
-                      channels += '<a href="https://www.YouTube.com/' + id + '"><i class="fa fa-youtube-play social-fa"></i></a>';
+                      channels += 'YouTube '+id+',';
                     }
 
                   }
@@ -299,7 +405,7 @@ $(function () {
                 if(officials[i].phones) {
                   for ( var k = 0; k < officials[i].phones.length; k++ ) {
                     phone = officials[i].phones[k] || '';
-                    phones += '<a href="tel:'+phone+'"><i class="fa fa-tty social-fa"></i></a>';
+                    phones += phone+', ';
                   }
                 }
 
@@ -309,17 +415,17 @@ $(function () {
                 if(officials[i].urls){
                   for ( var l = 0; l < officials[i].urls.length; l++ ) {
                     url = officials[i].urls[l] || '';
-                    urls += '<a href="'+url+'"><i class="fa fa-bookmark social-fa"></i></a>';
+                    urls += url+', ';
                   }
                 }
                 /*jshint multistr: true */
                  $reps.append('<div class="col-xs-12 col-sm-6 col-md-3 rep"> \
                               <div class="row"> \
                                 <div class="col-md-12 col-sm-3 col-xs-2">\
-                                  <span class="photo-container"><img src="'+photoUrl+'"></span>\
+                                  <span class="photo-container"><img data-party="'+party+'" data-name="'+name+'" data-photourl="'+photoUrl+'" data-position="'+position+'" data-channels="'+channels+'" data-phones="'+phones+'" data-urls="'+urls+'"class="rep-img" src="'+photoUrl+'"></span>\
                                 </div>\
                                 <div class="col-md-12 col-sm-9 col-xs-10">\
-                                  <div class="row rep-info text-center"><p>'+position+'</p><p>'+party+'</p><p>'+name+'</p><p>'+phones+urls+'</p><p>'+channels+'</p></div>\
+                                  <div class="row rep-info text-center"><p>'+party+'</p><p>'+name+'</p></div>\
                                 </div>\
                               </div>\
                             </div>');
@@ -331,13 +437,10 @@ $(function () {
 
 
               }
-
+              this.repListener();
               var $rep = $('.rep');
               var $repsInfo = $('.rep-info p');
-              var $icons = $('.social-fa');
-              //var $facebook = $('.fa-facebook-square');
-              var $youtube = $('.fa-youtube-play');
-              var $twitter = $('.fa-twitter');
+
               var $repImage = $('.photo-container img');
               var $pc = $('.photo-container');
 
@@ -370,27 +473,17 @@ $(function () {
 
 
               $rep.css({
-                marginBottom:'20px',
-                height: '300px'
+                marginBottom:'20px'
+
               });
 
               $repImage.css({
-                width: '90px'
+                width: '90px',
+                cursor:'pointer'
 
               });
 
-              $icons.css({
-                fontSize: '20px',
-                paddingRight: '10px'
-              });
 
-              $youtube.css({
-                color: 'red'
-              });
-
-              $twitter.css({
-                color: 'rgb(0,153,153)'
-              });
 
 
 
