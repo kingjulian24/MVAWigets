4afeddde113b44fd580c6900db0fa45d739f6502
Julian Brown <brownjulian2@gmail.com>
2014-12-04 16:15:28 -0500

    fix bl bugs

--- js/blscript2.js
+++ js/blscript2.js
@@ -1,13 +1,13 @@
 'use strict';
 /*
-    ============================================
+============================================
 
-    -Required: Jquery & bootstrap css
-    -Configuration line 191
+-Required: Jquery & bootstrap css
+-Configuration line 191
 
-    -The widget save the user's address on line 66
+-The widget save the user's address on line 66
 
-    ============================================
+============================================
 */
 
 $(function(){
@@ -29,71 +29,68 @@ $(function(){
 					class:'glyphicon glyphicon-refresh glyphicon-refresh-animate'
 				});
 
-				// if(this.paramCheck()){
-				// 	var params = document.URL.split('?')[1];
-				// 	var address = params.split('=')[1].split('%20').join(' ');
-				// 	this.$inputfield.val(address);
-				// 	this.sendAjaxRequest();
-				// }
+				this.sendAjaxRequest(); // initailize
+
 				this.addListener();
 			},
 			paramCheck: function(){ // Check for query string within url
 				if ( document.URL.indexOf('?') > -1 ) {
-				  return true;
+					return true;
 				} else {
-				  return false;
+					return false;
 				}
 			},
 			buildLayout: function(){
 				/*jshint multistr: true */
 				var layout = '<div class="pl-body col-xs-12"> \
-							      <h4 class="pl-title">Ballot Information Finder</h4> \
-							      <div class="row"> \
-							        <div class="col-xs-12 col-md-6"> \
-											<div><label for="input.address" style="text-indent:-10000px; position:absolute;">Enter Your Full Registered Address</label></div> \
-							        <form class="pl-form">\
-							          <div class="input-group"> \
-							            <input type="text" id="input.address" class="form-control pl-user-input"  placeholder="Enter full registered voting address" required tabindex = "1"> \
-							            <span class="input-group-btn"> \
-							              <button class="btn btn-danger" type="submit" id="pl-search-btn" tabindex = "2">Search</button> \
-							            </span> \
-							          </div> \
-							          </form>\
-							        </div> \
-							      </div> \
-										<br> \
-										<div id="accordion"> </div> \
-						      </div>';
+				<h4 class="pl-title">Ballot Information Finder</h4> \
+				<div class="row"> \
+				<div class="col-xs-12 col-md-6"> \
+				<div><label for="input.address" style="text-indent:-10000px; position:absolute;">Enter Your Full Registered Address</label></div> \
+				<form class="pl-form">\
+				<div class="input-group"> \
+				<input type="text" id="input.address" class="form-control pl-user-input"  value="5000 forbes ave pittsburgh PA 15213" placeholder="Enter full registered voting address" required tabindex = "1"> \
+				<span class="input-group-btn"> \
+				<button class="btn btn-danger" type="submit" id="pl-search-btn" tabindex = "2">Search</button> \
+				</span> \
+				</div> \
+				</form>\
+				</div> \
+				</div> \
+				<br> \
+				<div id="accordion"> </div> \
+				</div>';
 
 
-								//modal
-								layout	+= '<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
-									  <div class="modal-dialog">\
-									    <div class="modal-content">\
-									      <div class="modal-header">\
-									        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\
-									        <h4 class="modal-title" id="myModalLabel">Modal title</h4>\
-									      </div>\
-									      <div class="modal-body">\
-									        <div class="row">\
-														<div class="col-xs-4 photoUrl">\
-															<img src="http://satyaflowyoga.com/satyaflow/wp-content/uploads/2014/06/placeholder1.jpg" style="max-width:100%">\
-														</div>\
-														<div class="col-xs-8 candidateInfo">\
-														</div>\
-													</div>\
-									      </div>\
-									      <div class="modal-footer">\
-									        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
-									      </div>\
-									    </div>\
-									  </div>\
-									</div>';
-	  			this.$target.append(layout);
+				//modal
+				layout	+= '<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+				<div class="modal-dialog">\
+				<div class="modal-content">\
+				<div class="modal-header">\
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\
+				<h4 class="modal-title" id="myModalLabel">Modal title</h4>\
+				</div>\
+				<div class="modal-body">\
+				<div class="row">\
+				<div class="col-xs-4 photoUrl">\
+				<img src="http://satyaflowyoga.com/satyaflow/wp-content/uploads/2014/06/placeholder1.jpg" style="max-width:100%">\
+				</div>\
+				<div class="col-xs-8 candidateInfo">\
+				</div>\
+				</div>\
+				</div>\
+				<div class="modal-footer">\
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+				</div>\
+				</div>\
+				</div>\
+				</div>';
+				this.$target.append(layout);
 			},
 			addListener: function() {
 				this.firsttime = true;
 				this.$form.on( 'submit', function(e){
+
 					blWidget.sendAjaxRequest();
 					e.preventDefault();
 				});
@@ -197,38 +194,38 @@ $(function(){
 				var $targetInner = $('#accordion');
 				var buildHTML = '';
 
-				console.log(data);
+				//console.log(data);
 				if(data.contests.length > 0){ // validate data
 
 					var edata = data; //convert json to javascript object
 
-						for (var i = edata.contests.length - 1; i >= 0; i--) { // loop through data
-							var electionType = edata.contests[i].type;
-							var office = edata.contests[i].office;
-							buildHTML += '<h2  class="ctrl">'+office+' ('+electionType +')'+'</h2>';
-							var district = edata.contests[i].district;
-							var candidates = edata.contests[i].candidates;
-							buildHTML += '<div class="pnl">';
-							buildHTML += '<h5 >'+ district.name+' ('+district.scope+')'+'</h5>';
-							for (var j = 0; j < candidates.length; j++) {
-								var party = candidates[j].party;
-								buildHTML += '<h4 data-candidatename="'+candidates[j].name+'" class="myModal accordian-content panel-body" style="cursor: pointer;">'+' ' + candidates[j].name +' ('+party+')</h4>';
+					for (var i = edata.contests.length - 1; i >= 0; i--) { // loop through data
+						var electionType = edata.contests[i].type;
+						var office = edata.contests[i].office;
+						buildHTML += '<h2  class="ctrl">'+office+' ('+electionType +')'+'</h2>';
+						var district = edata.contests[i].district;
+						var candidates = edata.contests[i].candidates;
+						buildHTML += '<div class="pnl">';
+						buildHTML += '<h5 >'+ district.name+' ('+district.scope+')'+'</h5>';
+						for (var j = 0; j < candidates.length; j++) {
+							var party = candidates[j].party;
+							buildHTML += '<h4 data-candidatename="'+candidates[j].name+'" class="myModal accordian-content panel-body" style="cursor: pointer;">'+' ' + candidates[j].name +' ('+party+')</h4>';
 
-								this.candidates[candidates[j].name] = {
-										name:candidates[j].name,
-										party: party || '',
-										channels: candidates[j].channels || '',
-										email: candidates[j].email || '',
-										candidateUrl: candidates[j].candidateUrl || '',
-										photoUrl: candidates[j].photoUrl || '',
-										office: office || ''
-									};
+							this.candidates[candidates[j].name] = {
+								name:candidates[j].name,
+								party: party || '',
+								channels: candidates[j].channels || '',
+								email: candidates[j].email || '',
+								candidateUrl: candidates[j].candidateUrl || '',
+								photoUrl: candidates[j].photoUrl || '',
+								office: office || ''
+							};
 
 
-							}
-							buildHTML += '<h4  class="accordian-content panel-body">'+ ' ' + '___________ ' +'(Write-in)'+'</h4>';
-							buildHTML += '</div>';
 						}
+						buildHTML += '<h4  class="accordian-content panel-body">'+ ' ' + '___________ ' +'(Write-in)'+'</h4>';
+						buildHTML += '</div>';
+					}
 
 
 				} else { // if no data
@@ -237,45 +234,46 @@ $(function(){
 				$('.glyphicon-refresh').remove();// remove loading
 
 				this.displayData($targetInner,buildHTML);
-				this.addListener();
+				if(this.firsttime){this.addListener();}
 
-				if(this.firsttime){
-					$( '#accordion' ).accordion({ heightStyle: 'content'  });
-					this.firsttime = false;
-				} else {
-					$( '#accordion' ).accordion('destroy').accordion({ heightStyle: 'content'  });
+					if(this.firsttime){
+						$( '#accordion' ).accordion({ heightStyle: 'content'  });
+						this.firsttime = false;
+					} else {
+						$( '#accordion' ).accordion('destroy').accordion({ heightStyle: 'content'  });
+					}
+
+					this.$loading.remove();// remove loading
+
+				},
+				displayData: function($targetInner,data){
+					$targetInner.html('');
+					$targetInner.append(data);
+
+
+
 				}
 
-				this.$loading.remove();// remove loading
 
-			},
-			displayData: function($targetInner,data){
-				$targetInner.append(data);
+			}; // end of blWidget object
 
+			/*
+			============================================
 
+			Ballot Information Widget Config
+			Target = div to target on page(require id or class)
+			apiUrl = url to google civic api
+			apiKey = google civic api key (browser key)
+			electionId = id of interested election(found on google civic website)
 
-			}
+			============================================
+			*/
+			blWidget.init({
+				target: '#target-practice',
+				apiUrl: 'https://www.googleapis.com/civicinfo/v2/voterinfo?',
+				apiKey: 'AIzaSyDqyAn7yBGwWyZsFs5zWSh6zArNcQJDaAw',
+				electionId: '2000'
+			});
 
-
-		}; // end of blWidget object
-
-		/*
-		    ============================================
-
-		    Ballot Information Widget Config
-		    Target = div to target on page(require id or class)
-		    apiUrl = url to google civic api
-		    apiKey = google civic api key (browser key)
-		    electionId = id of interested election(found on google civic website)
-
-		    ============================================
-		*/
-		blWidget.init({
-			target: '#target-practice',
-			apiUrl: 'https://www.googleapis.com/civicinfo/v2/voterinfo?',
-			apiKey: 'AIzaSyDqyAn7yBGwWyZsFs5zWSh6zArNcQJDaAw',
-			electionId: '2000'
-		});
-
-	})(); // end of self invoking function
-});
+		})(); // end of self invoking function
+	});
